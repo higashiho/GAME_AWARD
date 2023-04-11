75b56e1f5db76faf1cd45e13618372739ba357ef
higashihokakeru <kakeru.0422.pojr@gmail.com>
2023-04-11 18:37:44 +0900

    TitleScene修正
    playerグラフィック差し込み　ray導入

--- 20230411_Food/Assets/Script/OutGame/OutGameConstants.cs
+++ 20230411_Food/Assets/Script/OutGame/OutGameConstants.cs
@@ -9,7 +9,12 @@ namespace Constants
     /// </summary>
     public abstract class OutGameConstants
     {
-        
+        /// <summary>プレイヤー左向き</summary>
+        public static readonly Vector3 PLAYER_DIRECTION_LEFT = new Vector3(0, 270, 0);
+        /// <summary>プレイヤー右向き</summary>
+        public static readonly Vector3 PLAYER_DIRECTION_RIGHT = new Vector3(0, 90, 0);
+        /// <summary>プレイヤー後ろ向き</summary>
+        public static readonly Vector3 PLAYER_DIRECTION_BACK = new Vector3(0, 180, 0);
     }
 }
 
