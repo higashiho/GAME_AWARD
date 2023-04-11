bb181358c2aaeb1409265a39bbb27030ebc12900
ryomayukumoto <ryomayukkun@gmail.com>
2023-04-11 16:01:59 +0900

    FoodPointManagerコメント追加

--- 20230411_Food/Assets/Script/InGame/FoodPointManager.cs
+++ 20230411_Food/Assets/Script/InGame/FoodPointManager.cs
@@ -2,7 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FoodPointManager : MonoBehaviour
+namespace FoodPoint
 {
-    
+    public class FoodPointManager 
+    {
+        private FoodPoint sumPlayer1FoodPoint;
+        private FoodPoint sumPlayer2FoodPoint;
+
+
+        // FoodPointの多いプレイヤーを取得するメソッド
+        public void GetWinner()
+        {
+            // PlayerのSumFoodpointを比較して
+            // 大きい方のプレイヤーを返す
+            // Mathクラス
+        }
+
+        // 例)  fishPoint
+        //
+        // 指定された料理のfishPointと比較して割合を取得する
+        // 割合 < 1　指定された料理のfishPoint * 割合  => FoodPointに加算
+        // 割合 > 1  指定された料理のfishpoint * (割合 - 1)  => FoodPointに加算
+
+    }
 }
+
